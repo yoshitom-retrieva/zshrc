a4d94813a28b1bc9136c29183be3152cbb629687
yoshitom <borzoi_forte_apple@msn.com>
2022-12-14 12:10:12 +0900

    add japanese ime setting

---
+++ bin/setup_japanise_ime.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+# Last Change:2022/12/14.
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+sudo apt update
+sudo apt install -y fcitx-bin fcitx-mozc dbus-x11 language-pack-ja
+sudo update-locale LANG=ja_JP.UTF8
+
+source ${SCRIPT_DIR}/../.zsshrc.win.wsl
