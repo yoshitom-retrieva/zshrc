4e5370292a3cc017c8d20c0fa7d2f41c6b3dd888
yoshitom <borzoi_forte_apple@msn.com>
2022-12-14 13:00:24 +0900

    add japanese font rep

--- bin/setup_japanise_ime.sh
+++ bin/setup_japanise_ime.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 sudo apt update
-sudo apt install -y fcitx-bin fcitx-mozc dbus-x11 language-pack-ja
+sudo apt install -y fcitx-bin fcitx-mozc dbus-x11 language-pack-ja fonts-noto
 sudo update-locale LANG=ja_JP.UTF8
 
 source ${SCRIPT_DIR}/../.zsshrc.win.wsl
