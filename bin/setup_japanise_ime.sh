90f04112c6b7276efd8707f67c1eafc15173e2b7
yoshitom <borzoi_forte_apple@msn.com>
2024-11-07 14:56:31 +0900

    fix for japanese ime to start with start icon

--- bin/setup_japanise_ime.sh
+++ bin/setup_japanise_ime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# Last Change:2022/12/14.
+# Last Change:2024/11/07.
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
@@ -7,4 +7,6 @@ sudo apt update
 sudo apt install -y fcitx-bin fcitx-mozc dbus-x11 language-pack-ja fonts-noto
 sudo update-locale LANG=ja_JP.UTF8
 
-source ${SCRIPT_DIR}/../.zsshrc.win.wsl
+cat << EOS >> ~/.profile
+source ${SCRIPT_DIR}/../.zshrc.win.wsl
+EOS
